1537ec203069a2eb4216a24c0f834b8691d684e1
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-04-10 21:54:18 +0100

    updated new controller (SearchparamsController) in SearchEnginesAPIs project for returning all endpoints of all google searchengines //10-04-2024

---
+++ SearchEngineAPIs/Controllers/SearchparamsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http;
+using System.Web.Routing;
+namespace SearchEngineAPIs.Controllers
+{
+    public class SearchparamsController : ApiController
+    {
+        [MyAuthorizationFilter]
+        public IHttpActionResult Get([FromUri]Filter filter)
+        {
+
+        }
+        public DataTable retrieveSearchEngines()
+        {
+            string conStr = ConfigurationManager.ConnectionStrings["myConnectionString"].ToString();
+
+            SqlConnection cn = new SqlConnection(conStr);
+            cn.Open();
+            DataTable dt = new DataTable();
+            using (SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM SearchEnginesList", cn))
+            {
+                da.Fill(dt);
+            }
+            cn.Close();
+            return dt;
+        }
+
+    }
+}
